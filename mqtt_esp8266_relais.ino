e611f4e75f07b6546273dad419887c39c61262e1
domi77185 <tel.domi@gmail.com>
2020-01-04 14:56:24 +0100

    Update mqtt_esp8266_relais.ino

--- mqtt_esp8266_relais.ino
+++ mqtt_esp8266_relais.ino
@@ -38,7 +38,7 @@ const char* password = "_MOT_DE_PASSE_WIFI_";   // Mot de passe du réseau Wifi.
 const char* mqtt_server = "_IP_DU_BROKER_";     // Adresse IP ou DNS du Broker.
 const int mqtt_port = 1883;                     // Port du Brocker MQTT
 const char* mqtt_login = "_LOGIN_";             // Login de connexion à MQTT.
-const char* mqtt_password = "8PASSWORD_";       // Mot de passe de connexion à MQTT.
+const char* mqtt_password = "_PASSWORD_";       // Mot de passe de connexion à MQTT.
 // ------------------------------------------------------------
 // Variables et constantes utilisateur :
 String nomModule = "Module relais";     // Nom usuel de ce module.

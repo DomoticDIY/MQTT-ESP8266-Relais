846a734392c7153b8eb03a164cc66532ac74e3b7
domi77185 <tel.domi@gmail.com>
2020-03-24 10:29:59 +0100

    Update mqtt_esp8266_relais.ino

--- mqtt_esp8266_relais.ino
+++ mqtt_esp8266_relais.ino
@@ -6,7 +6,7 @@
 
  Bibliothéques nécessaires :
  - pubsubclient : https://github.com/knolleary/pubsubclient
- - ArduinoJson : https://github.com/bblanchon/ArduinoJson
+ - ArduinoJson v5.13.3 : https://github.com/bblanchon/ArduinoJson
 Télécharger les bibliothèques, puis dans IDE : Faire Croquis / inclure une bibliothéque / ajouter la bibliothèque ZIP.
 Dans le gestionnaire de bibliothéque, charger le module ESP8266Wifi.
 
